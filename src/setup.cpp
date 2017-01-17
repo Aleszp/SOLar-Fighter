5d0dd4e8b9cd5db890410f27e103750a8d4b6f4b
Aleszp <aleksander.szsz@gmail.com>
2017-01-17 17:17:23 +0100

    Minimalne modyfikacje programu konfiguracyjnego.

--- src/setup.cpp
+++ src/setup.cpp
@@ -1,6 +1,8 @@
 #include <ncurses.h>
 #include <cstdio>
 
+enum pos {AUTODETECT=1, RES_X=2, RES_Y=3,DEPTH=4,DBL_BUFFED=5,WINDOWED=6,SAVE=8, EXIT=9}l; 
+
 int main(int argc, char** argv)
 {
 	initscr ();
@@ -8,6 +10,9 @@ int main(int argc, char** argv)
 	noecho ();
 	start_color();
 	keypad(stdscr, TRUE);	
+	
+	init_pair(1, COLOR_WHITE, COLOR_BLUE);
+	wbkgd(stdscr, COLOR_PAIR(1));
 	
 	int ch;
 	
@@ -28,7 +33,7 @@ int main(int argc, char** argv)
 		
 	if(!config)
 	{
-		printw("Nie znaleziono pliku konfiguracyjnego :(.\nZostanie utworzony nowy.\n");
+		printw("Nie znaleziono pliku konfiguracyjnego :(. Zostanie utworzony nowy.\n");
 	}
 	else
 	{
@@ -48,7 +53,7 @@ int main(int argc, char** argv)
 	}
 
 	nodelay(stdscr,true);
-	int offset=2;
+	int offset=1;
 	int ptr_max=7;
 	int ptr=offset;
 	int old=ptr;
@@ -75,13 +80,13 @@ int main(int argc, char** argv)
 		
 		if(ch==' '||ch==KEY_LEFT||ch==KEY_RIGHT)	//spacja
 		{
-			if(ptr==2)
+			if(ptr==AUTODETECT)
 				autodetect?autodetect=false:autodetect=true;
 			
-			if(ptr==6)
+			if(ptr==DBL_BUFFED)
 				dbl_buff?dbl_buff=false:dbl_buff=true;
 			
-			if(ptr==7)
+			if(ptr==WINDOWED)
 				windowed?windowed=false:windowed=true;
 		}
 		if(ch==' '&&(ptr==6+offset))
@@ -115,52 +120,53 @@ int main(int argc, char** argv)
 			break;		
 		if(ch=='+'||ch=='=')
 		{
-			if(ptr==3)
+			if(ptr==RES_X)
 				res_x++;
-			if(ptr==4)
+			if(ptr==RES_Y)
 				res_y++;
-			if(ptr==5)
+			if(ptr==DEPTH)
 				depth++;
 		}
 		if(ch=='-')
 		{
-			if(ptr==3)
+			if(ptr==RES_X)
 				res_x--;
-			if(ptr==4)
+			if(ptr==RES_Y)
 				res_y--;
-			if(ptr==5)
+			if(ptr==DEPTH)
 				depth--;
 		}
 		if(ch==KEY_LEFT)
 		{
-			if(ptr==3)
+			if(ptr==RES_X)
 				res_x-=40;
-			if(ptr==4)
+			if(ptr==RES_Y)
 				res_y-=40;
-			if(ptr==5)
+			if(ptr==DEPTH)
 				depth-=8;
 		}
 		if(ch==KEY_RIGHT)
 		{
-			if(ptr==3)
+			if(ptr==RES_X)
 				res_x+=40;
-			if(ptr==4)
+			if(ptr==RES_Y)
 				res_y+=40;
-			if(ptr==5)
+			if(ptr==DEPTH)
 				depth+=8;
 		}
 		
 		mvprintw(old>5+offset?old+1:old, 0, " ");
 		mvprintw(ptr>5+offset?ptr+1:ptr, 0, "*");
 		old=ptr;
-		mvprintw(2, 2, "Autodetect:\t\t%s",autodetect?"ON ":"OFF");
-		mvprintw(3, 2, "Resolution x:\t\t"); autodetect?printw("AUTODETECT"):printw("%3i       ",res_x);
-		mvprintw(4, 2, "Resolution y:\t\t"); autodetect?printw("AUTODETECT"):printw("%3i       ",res_y);
-		mvprintw(5, 2, "Depth:\t\t"); autodetect?printw("AUTODETECT"):printw("%2i        ",depth);
-		mvprintw(6, 2, "Double buffered:\t%s",dbl_buff?"ON ":"OFF");
-		mvprintw(7, 2, "Windowed:\t\t%s",windowed?"ON ":"OFF");
-		mvprintw(9, 2, "Save and exit");
-		mvprintw(10, 2, "Exit without saving (you can press Q or ESC too)");
+		
+		mvprintw(AUTODETECT, 2, "Autodetect:\t\t%s",autodetect?"ON ":"OFF");
+		mvprintw(RES_X, 2, "Resolution x:\t\t"); autodetect?printw("AUTODETECT"):printw("%3i       ",res_x);
+		mvprintw(RES_Y, 2, "Resolution y:\t\t"); autodetect?printw("AUTODETECT"):printw("%3i       ",res_y);
+		mvprintw(DEPTH, 2, "Depth:\t\t"); autodetect?printw("AUTODETECT"):printw("%2i        ",depth);
+		mvprintw(DBL_BUFFED, 2, "Double buffered:\t%s",dbl_buff?"ON ":"OFF");
+		mvprintw(WINDOWED, 2, "Windowed:\t\t%s",windowed?"ON ":"OFF");
+		mvprintw(SAVE, 2, "Save and exit");
+		mvprintw(EXIT, 2, "Exit without saving (you can press Q or ESC too)");
 		
 		refresh();
 	}
